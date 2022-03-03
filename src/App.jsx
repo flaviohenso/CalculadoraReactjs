c6debacf4eb267ea18c02f6f75a4037ebb36a59b
flaviohenso <flavio.henso@gmail.com>
2022-03-02 22:52:36 -0300

    Projeto Calculadora

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Calculator from './main/Calculator';
+
+function App() {
+  return (<div>
+    <div>Calculadora</div>
+    <div className="App">
+      <Calculator/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
